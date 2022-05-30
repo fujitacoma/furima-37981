5936eef03daf65bcc58cee2791e3e9553ff97bba
fujitacoma <john.vsvk.0424@gmail.com>
2022-05-30 23:14:49 +0900

    バリデーション修正

--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -5,12 +5,12 @@ class PurchaseCustomer
     with_options presence: true do
       validates :postcode, :prefecture_id, :city, :block, :phone_number, :user_id, :item_id
     end
-      validates :prefecture_id,        numericality: { other_than: 1 }
+      validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank" }
       POSTCODE_RULE = /\A\d{3}[-]\d{4}\z/.freeze
       validates_format_of :postcode, with: POSTCODE_RULE, message: "is invalid. Enter it as follows (e.g. 123-4567)"
       PHONE_NUMBER_RULE = /\A\d{10,11}\z/.freeze
-      validates_format_of :phone_number, with: PHONE_NUMBER_RULE, message: 'is invalid. Input only number'
-      validates :user_id, :item_id, presence: true
+      validates_format_of :phone_number, with: PHONE_NUMBER_RULE, message: 'is too short'
+      validates :phone_number,         numericality: { message: 'is invalid. Input only number' }
 
     def save
       purchase = Purchase.create(user_id: user_id, item_id: item_id)
